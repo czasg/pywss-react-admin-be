fc5d9301055251fb7211f83fd3f9b3a76d0a111b
czasg <972542655@qq.com>
2023-04-24 15:30:22 +0800

    feat: init

--- api/system/role/__init__.py
+++ api/system/role/__init__.py
@@ -13,6 +13,25 @@ from utils.exception import StrException
 name_regex = re.compile("^[a-zA-Z]+$")
 
 
+class HttpGetRequest(BaseModel):
+    pageSize: int = 10
+    pageNum: int = 0
+    name: str = ""
+    alias: str = ""
+
+    def bind_query(self, query, ignore_page=False):
+        queries = [
+            (bool(self.name), UserRole.name.contains(self.name)),
+            (bool(self.alias), UserRole.alias.contains(self.alias)),
+        ]
+        for enable, query_filter in queries:
+            if enable:
+                query = query.filter(query_filter)
+        if not ignore_page:
+            query = query.limit(self.pageSize).offset(self.pageNum)
+        return query
+
+
 class HttpPostRequest(BaseModel):
     name: str
     alias: str
@@ -21,14 +40,15 @@ class HttpPostRequest(BaseModel):
 
 class RoleService:
 
-    def get_roles(self) -> List[UserRole]:
+    def get_roles(self, request: HttpGetRequest) -> List[UserRole]:
         with Session() as session:
             query = session.query(UserRole)
+            query = request.bind_query(query)
             return query.all()
 
     def add_role(self, role: UserRole):
         with Session() as session:
-            existRole = session.query(UserRole).filter(UserRole.name == role.name).first()
+            existRole = session.query(UserRole).filter(UserRole.name == role.name).scalar()
             if existRole:
                 raise StrException(f"角色[{role.name}]已存在")
             session.add(role)
@@ -40,6 +60,7 @@ class View(RoleService):
     @pywss.openapi.docs(summary="获取角色列表")
     def http_get(self, ctx: pywss.Context):
         resp = Response()
+        req = HttpGetRequest(**ctx.url_params)
         try:
             resp.data = [
                 {
@@ -50,7 +71,7 @@ class View(RoleService):
                     "created_at": role.created_at.strftime("%Y-%m-%d %H:%M:%S"),
                     "updated_at": role.updated_at.strftime("%Y-%m-%d %H:%M:%S"),
                 }
-                for role in self.get_roles()
+                for role in self.get_roles(req)
             ]
         except StrException as e:
             resp.code = 99999
@@ -70,7 +91,7 @@ class View(RoleService):
             return
         if not name_regex.match(req.name):
             resp.code = 99999
-            resp.message = "无效用户名，仅支持大小写字母"
+            resp.message = "无效角色名，仅支持大小写字母"
             ctx.write(resp)
             return
         role = UserRole(
--- service/role.py
+++ service/role.py
@@ -5,6 +5,13 @@ from db import Session
 from db.model import UserRole, UserRoleMid
 
 
+def get_roles():
+    with Session() as session:
+        return session.query(UserRole). \
+            order_by(UserRole.id). \
+            all()
+
+
 def get_user_roles(uid: int) -> List[UserRole]:
     with Session() as session:
         return session.query(UserRole). \
